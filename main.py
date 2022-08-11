03b3385101bde2beb48742524a821c3d27e6fe5a
Шатилова Д.В <19974240@cab-wsm-0061888.sigma.sbrf.ru>
2022-08-11 18:44:04 +0600

    f

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-values = ['@dar','Дарья','Шатилова','Toyota','Camry','a 456 rt','89503378718']
-str_question = ['?' for i in range(len(values))]
-print(tuple(str_question))
+
+from datetime import datetime
+s = datetime.now()
+print(type(s))
